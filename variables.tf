b3362523cd10a1f2845b712c47a9ddf62efcedd1
mbrow73 <fatsaltytryhard@gmail.com>
2025-06-21 12:25:34 -0700

    fixing selflink

--- variables.tf
+++ variables.tf
@@ -63,7 +63,39 @@ variable "ca_country_code" {
 }
 
 
-variable "inet_firewall_rules" {
+variable "auto_firewall_rules" {
+  description = <<-EOT
+    A list of firewall‐policy rules to create.  
+    Each object must include:
+      - name                   = unique name for the rule  
+      - description            = human description  
+      - priority               = rule priority (lower = higher match)  
+      - direction              = "INGRESS" or "EGRESS"  
+      - action                 = one of ["allow","deny","apply_security_profile_group"]  
+      - security_profile_group = (if action == "apply_security_profile_group")  
+      - enable_logging         = bool  
+      - src_ip_ranges          = list(string)  
+      - dest_ip_ranges         = list(string)  
+      - ports                  = list(string)  # layer4 ports  
+      - tls_inspection         = bool           # whether to decrypt TLS  
+  EOT
+  type = list(object({
+    name                   = string
+    description            = string
+    priority               = number
+    direction              = string
+    action                 = string
+    security_profile_group = optional(string)
+    enable_logging         = bool
+    src_ip_ranges          = list(string)
+    dest_ip_ranges         = list(string)
+    ports                  = list(string)
+    protocol               = string
+    tls_inspection         = bool
+  }))
+}
+
+variable "manual_firewall_rules" {
   description = <<-EOT
     A list of firewall‐policy rules to create.  
     Each object must include:
